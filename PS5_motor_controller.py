deabc2b88263f2107b751779412ff5ae8e396998
tphollis <thomas.preston.hollis@outlook.com>
2022-03-31 20:02:59 -0600

    Added better notes to code.

--- PS5_motor_controller.py
+++ PS5_motor_controller.py
@@ -18,7 +18,7 @@ from evdev import InputDevice, categorize, ecodes # access to PS5 controller
 
 
 #Global Variables
-GAMEPAD = InputDevice('/dev/input/event2')   # Location of controller inputs
+GAMEPAD = InputDevice('/dev/input/event4')   # Location of controller inputs ("cd /dev/input" then "ls -al")
 CENTER = 128                                 # Joystick center positoin for x and y axis
 DRIFT = 6                                    # Amount of padding around center to prevent drift
 
@@ -52,15 +52,18 @@ ANALOGS = {
     }
 
 
-#Translates analog stick values to values the motor can use
+'''...Translates analog stick values to values the motor can use...'''
 def translate_analog_stick(value):
+    #Motors can only go up to a power of 100.
+    #The analog sticks can vary from 0 - 255.
+    #Do math that makes it so full throttle up/down/left/right equals 100.
     translation = int((value-CENTER)*0.78)
     
     #Keep number positive
     if translation < 0:
         translation *= -1
     
-    #Number can not be gratter than 100
+    #Number can not be gratter than 100. Just a precausion.
     if translation > 100:
         translation = 100
     
@@ -69,26 +72,31 @@ def translate_analog_stick(value):
 
 #Runs the infinate loop activating code based on changes.
 def main():
-    #Create instances of motors
+    #The following shows GPIO numbers and their pin location on a raspberry pi.
     #pin:GPIO - 3:2, 5:3, 7:4, 8:14, 10:15, 11:17, 12:18, 13:27, 15:22, 16:23
     #           18:24, 19:10, 21:9, 22:25, 23:11, 24:8, 26:7, 29:5, 31:6, 32:12
     #           33:13, 35:19, 36:16, 37:26, 38:20, 40:21
+
+    #Create instances of motors.
+    #Motor(EN, IN1, IN2) - Use GPIO numbers.
     leftDrive = motor.Motor(21, 20, 26)
     rightDrive = motor.Motor(16, 19, 13)
     
-    #Get every event that happens on the controller
+    #Get every event that happens on the controller. (Infinate loop)
     for event in GAMEPAD.read_loop():
         
-        #Code if the event is a button type
+        #Code for button types go here inside this IF statement.
         if event.type == ecodes.EV_KEY and event.code in BUTTONS:
+
+            #When "circle" is pressed, stop the program.
             if BUTTONS[event.code] == "circle" and event.value == 1:
                 quit()
         
         
-        #Code if the event is an absolute type
+        #Code for analog types go there inside this IF statement
         if event.type == ecodes.EV_ABS and event.code in ANALOGS:
             
-            #If left analog stick is up, move left motors forword. 
+            #If left analog stick is used, do the following. 
             if ANALOGS[event.code] == "joystick_left_y":
                 if event.value > CENTER+DRIFT:
                     leftDrive.backword(translate_analog_stick(event.value), "left")
@@ -97,7 +105,7 @@ def main():
                 else:
                     leftDrive.stop("left")
                     
-            #If left analog stick is up, move left motors forword. 
+            #If right analog stick is used, do the following.
             if ANALOGS[event.code] == "joystick_right_y":
                 if event.value > CENTER+DRIFT:
                     rightDrive.backword(translate_analog_stick(event.value), "right")
@@ -107,5 +115,6 @@ def main():
                     rightDrive.stop("right")
 
 
+#Initiate the program with main().
 if __name__ == "__main__":
     main()
--- motor.py
+++ motor.py
@@ -21,12 +21,14 @@ GPIO.setwarnings(False)
 
 
 class Motor():
+
+    '''...Initializer...'''
     def __init__(self, EN, IN1, IN2):
         
         #Preset variables
-        self.EN = EN
-        self.IN1 = IN1
-        self.IN2 = IN2
+        self.EN = EN        #Allows speed control
+        self.IN1 = IN1      #IN1 and IN2 determine direction
+        self.IN2 = IN2      #   of motor direction.
         
         #GPIO setup
         GPIO.setup(self.EN, GPIO.OUT)
@@ -36,21 +38,38 @@ class Motor():
         #Define pwm (Pulse Width Modulation)
         self.pwm = GPIO.PWM(self.EN, 100)
         self.pwm.start(0)
-        
-    def forword(self, speed, side):
+
+
+    '''...Move motor Clockwise...'''
+    def clockwise(self, speed, motor = "..."):
+        #'speed' is a number used to specify power put into motor.
+        #'motor' is a string used to specify which motor is being used.(optional)
         GPIO.output(self.IN1, GPIO.LOW)
         GPIO.output(self.IN2, GPIO.HIGH)
         self.pwm.ChangeDutyCycle(speed)
-        print("Moving {} side forword at speed {}".format(side, speed))
-        
-    def backword(self, speed, side):
+
+        #Print which motor is being affected.
+        print("Moving {} motor clockwise at speed {}".format(motor, speed))
+
+
+    '''...Move motor counterclockwise...'''
+    def counterclockwise(self, speed, motor = "..."):
+        #'speed' is a number used to specify power put into motor.
+        #'motor' is a string used to specify which motor is being used.(optional)
         GPIO.output(self.IN1, GPIO.HIGH)
         GPIO.output(self.IN2, GPIO.LOW)
         self.pwm.ChangeDutyCycle(speed)
-        print("Moving {} side back at speed {}".format(side, speed))
-        
-    def stop(self, side):
+
+        #Print which motor is being affected.
+        print("Moving {} motor counterclockwise at speed {}".format(motor, speed))
+
+
+    '''...Stop motor from running...'''
+    def stop(self, motor = "..."):
+        #'motor' is a string used to specify which motor is being used. (optional)
         GPIO.output(self.IN1, GPIO.HIGH)
         GPIO.output(self.IN2, GPIO.LOW)
         self.pwm.ChangeDutyCycle(0)
-        print("{} side at stop".format(side))
+
+        #Print which motor is being affected.
+        print("{} motor at stop".format(motor))
